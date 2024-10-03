ad63104de48a7938e025163e73b5b3229ff4ba88
DenisSavytskiy <denys.savytskyi.ri.2024@lpnu.ua>
2024-10-03 06:37:06 +0300

    Lab№5.2

---
+++ Lab№5.2.1/Lab№5.2.1.cpp
@@ -0,0 +1,112 @@
+﻿//// Лабораторна робота № 5.2
+//#define M_PI 3.14159265358979323846
+//#include <iostream>
+//#include <iomanip>
+//#include <cmath>
+//using namespace std;
+//void S(const double x, const double eps, int& n, double& s);
+//void A(const double x, const int n, double& a);
+//int main()
+//{
+//	double  xp, xk, x, dx, eps, s = 0;
+//	int n = 0;
+//	cout << "xp = "; cin >> xp;
+//	cout << "xk = "; cin >> xk;
+//	cout << "dx = "; cin >> dx;
+//	cout << "eps = "; cin >> eps;
+//	cout << fixed;
+//	cout << "-------------------------------------------" << endl;
+//	cout << "|" << setw(5) << "x" << "   |"
+//		<< setw(10) << "arcctg(x)" << " |"
+//		<< setw(7) << "S" << "    |"
+//		<< setw(5) << "n" << "   |"
+//		<< endl;
+//	cout << "-------------------------------------------" << endl;
+//	x = xp;
+//	while (x <= xk)
+//	{
+//		S(x, eps, n, s);
+//		cout << "|" << setw(7) << setprecision(2) << x << " |"
+//			<< setw(10) << setprecision(5) << atan(1 / x) << " |"
+//			<< setw(10) << setprecision(5) << s << " |"
+//			<< setw(7) << n << " |"
+//			<< endl;
+//		x += dx;
+//	}
+//	cout << "-------------------------------------------" << endl;
+//	return 0;
+//}
+//void S(const double x, const double eps, int& n, double& s)
+//{
+//	n = 0;
+//	double a = -x;
+//	s = (x < 0) ?  -x - (M_PI / 2) : -x + (M_PI / 2);
+//	do {
+//		n++;
+//		A(x, n, a);
+//		s += a;
+//	} while (abs(a) >= eps);
+//}
+//void A(const double x, const int n, double& a)
+//{
+//	double R = -(x * x) * (2 * n - 1) / (2 * n + 1); 
+//	a *= R;
+//}
+
+
+// Лабораторна робота № 5.2
+#define M_PI 3.14159265358979323846
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+int main()
+{
+	double xp, xk, x, dx, eps, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << "   |"
+		<< setw(10) << "arcctg(x)" << " |"
+		<< setw(7) << "S" << "    |"
+		<< setw(5) << "n" << "  |"
+		<< endl;
+	cout << "------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << atan(1 / x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(7) << n << "|"
+			<< endl;
+		x += dx;
+	}
+	cout << "------------------------------------------" << endl;
+	return 0;
+}
+double S(const double x, const double eps, int& n, double s)
+{
+	n = 0;
+	double a = -x;
+	s = (x < 0) ?  -x - (M_PI / 2) : -x + (M_PI / 2);
+	do {
+		n++;
+		a = A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
+double A(const double x, const int n, double a)
+{
+	double R = -(x * x) * (2 * n - 1) / (2 * n + 1);
+	a *= R;
+	return a;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,33 @@
+﻿#define M_PI 3.14159265358979323846
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab№5.2.1/Lab№5.2.1.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double eps = 0.00001;
+			double s = 0;
+			int n = 0;
+
+			S(-1, eps, n, s);
+			Assert::AreEqual(s, atan(1 / -1), 0.00001);
+
+			S(-0.5, eps, n, s);в
+			Assert::AreEqual(s, atan(1 / -0.5), 0.00001);
+
+			S(0.5, eps, n, s);
+			Assert::AreEqual(s, atan(1 / 0.5), 0.00001);
+
+			S(1, eps, n, s);
+			Assert::AreEqual(s, atan(1 / 1), 0.00001);
+		}
+	};
+}
